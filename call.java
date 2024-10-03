2c493303526eb5a5f51242c2e29edd16a818de58
akhireddi <akhilagreddy.22@gmail.com>
2024-10-03 11:04:08 +0530

    nothing

---
+++ call.java
@@ -0,0 +1,13 @@
+public class Method{
+	
+	public static void main(String[]args){
+	    mul(10);
+	    mul(5);
+	}
+
+public static void mul(int number1,int number2){
+	int multipliedvalue=number1*number2; 
+       System.out.println(multipliedvalue);
+
+	}
+}
