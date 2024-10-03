b7ae52fce45fd5c08c2f64fd0fc36814e49d2166
akhireddi <akhilagreddy.22@gmail.com>
2024-10-03 10:15:40 +0530

    something

---
+++ data.java
@@ -0,0 +1,37 @@
+public class data{
+	 
+	 public static void main(String[] args){
+
+	        long aadharNum=431374863245l;
+	        long phoneNum=6362459960l;
+	        long pancardNum=2345672345l;
+
+	        float height=5.3f;
+	        float c=26.5f;
+	        float gravity=9.8f;
+
+            double pi=3.142987d;
+            double num=10.52d;
+
+            boolean hadUrDinner=true;
+            boolean areUReady=false;
+            boolean didUGetTheParcel=true;
+
+            char gender='F';
+            char result='P';
+           
+           System.out.println(aadharNum);
+           System.out.println(phoneNum);
+           System.out.println(pancardNum);
+           System.out.println(height);
+           System.out.println(height);
+           System.out.println(gravity);
+           System.out.println(pi);
+           System.out.println(num);
+           System.out.println(hadUrDinner);
+           System.out.println(areUReady);
+           System.out.println(didUGetTheParcel);
+           System.out.println(gender);
+           System.out.println(result);
+	        }
+}
